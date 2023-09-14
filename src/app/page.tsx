754c45f0f8a5c1886051d0d8822dc34828934ffe
yuan <fatfishman90@gmail.com>
2023-09-14 19:58:33 +0800

    fix: update icon setting for mobile screen.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           wellcome to &nbsp;
           <code className="font-mono font-bold">Artela playground</code>
         </p> */}
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none text-white flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://artela.network/"
