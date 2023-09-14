793980928a87f8564605c65407e78beaeae0a99c
Jack Li <jack@artela.network>
2023-09-14 19:44:45 +0800

    fix: fix display on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
 
-    <main className="flex min-h-screen flex-col items-center justify-between p-30">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex-auto sm:flex-auto">
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           wellcome to &nbsp;
           <code className="font-mono font-bold">Artela playground</code>
@@ -33,14 +33,14 @@ export default function Home() {
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"> */}
       <div className="relative flex py-8 place-items-center before:absolute before:h-[300px] before:w-[480px]">
         {/* <h1 className={`mb-3 text-2xl font-semibold`}>Artela Dev Net</h1> */}
-        <code className="font-mono font-bold text-5xl text-white">Artela Devnet</code>
+        <code className="font-mono font-bold text-5xl text-white text-center">Artela Devnet</code>
       </div>
       {/* <div className="relative flex py-1 place-items-center before:absolute before:h-[300px] before:w-[480px]">
         <code className="font-mono text-1xl text-white">Artela Developer Portal</code>
       </div> */}
 
       <div className="relative flex py-1 place-items-center before:absolute before:h-[300px] before:w-[480px]">
-        <code className="font-mono text-1xl text-white">Curated Developer Resources to <span style={{ color: '#0082ff' }}>#BuildOnArtela</span></code>
+        <code className="font-mono text-1xl text-white text-center">Curated Developer Resources to <span style={{ color: '#0082ff' }}>#BuildOnArtela</span></code>
         {/* <code className="text-blue"></code> */}
       </div>
 
@@ -126,7 +126,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Aspect tools for boosting the development of Aspect and smart contract. 
+            Aspect tools for boosting the development of Aspect and smart contract.
           </p>
         </a>
 
@@ -194,7 +194,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            An enhanced version of SOLC to compile smart contracts 
+            An enhanced version of SOLC to compile smart contracts
           </p>
         </a>
 
@@ -251,14 +251,14 @@ export default function Home() {
       </div>
 
       <div className="relative flex py-8 place-items-center before:absolute before:h-[300px] before:w-[480px]">
-        <code className="font-mono font-bold text-5xl text-white">New World with Aspect</code>
+        <code className="font-mono font-bold text-5xl text-white text-center">New World with Aspect</code>
       </div>
-      
+
       <div className="border-2 border-white	 w-3/5"></div>
-      <div className="mb-32 grid gap-5 items-stretch text-center py-20 lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <div className="mb-32 grid gap-5 items-stretch text-center py-20 grid-cols-1 lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
           href="https://medium.com/@artela_network/eliminate-reentrancy-attacks-with-on-chain-runtime-protection-6fb1142945a7"
-          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 w-full h-auto min-h-0 lg:min-h-400 lg:h-200 transition-colors hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -272,10 +272,10 @@ export default function Home() {
             Eliminate reentrancy attacks with on-chain runtime protection
           </p>
         </a>
-        
+
         <a
           href="https://www.youtube.com/watch?v=jkrELgwtykk&t=370s"
-          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 w-full h-auto min-h-0 lg:min-h-400 lg:h-200 transition-colors hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -289,10 +289,10 @@ export default function Home() {
             Empowering on-chain functionality for intent-centric protocols
           </p>
         </a>
-       
+
         <a
           // href="http://docs.artela.network/main"
-          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 w-full h-auto min-h-0 lg:min-h-400 lg:h-200 transition-colors hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -303,10 +303,10 @@ export default function Home() {
             On-chain just-in-time operations such as JIT liquidation, JIT LP management, and MEV-capturing AMM.
           </p>
         </a>
-        
+
         <a
           // href="http://docs.artela.network/main"
-          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 w-full h-auto min-h-0 lg:min-h-400 lg:h-200 transition-colors hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -317,10 +317,10 @@ export default function Home() {
            Subscribe to real-time specific on-chain events to trigger atomic tasks.
           </p>
         </a>
-       
+
         <a
           // href="http://docs.artela.network/main"
-          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 w-full h-auto min-h-0 lg:min-h-400 lg:h-200 transition-colors hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -331,10 +331,10 @@ export default function Home() {
            Upgrade game equipment with enhanced programmability
           </p>
         </a>
-        
+
         <a
           // href="http://docs.artela.network/main"
-          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 pt-8 pb-20 w-full h-auto min-h-0 lg:min-h-400 lg:h-200 transition-colors hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -345,10 +345,10 @@ export default function Home() {
             Public on-chain services with modular upgrades.
           </p>
         </a>
-        
+
         <a
           href="https://t.me/artela_official"
-          className="group col-span-3 rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border lg:col-span-3 border-transparent px-5 pt-8 pb-20 w-full h-auto min-h-0 lg:min-h-400 lg:h-200 transition-colors hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -358,16 +358,16 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`text-sm text-white opacity-90 text-center `}>
+          <p className={`text-sm text-white opacity-90 text-center`}>
             Contribute your research and ideas to the Artela Community. Let’s build the feature-rich dApps together!
           </p>
         </a>
       </div>
-{/* 
+{/*
       <div className="relative flex py-8 place-items-center before:absolute before:h-[300px] before:w-[480px]">
         <code className="font-mono font-bold text-5xl text-white">Contribute to Artela</code>
       </div>
-      
+
       <div className="border-2 border-white	 w-3/5"></div>
 
       <div className="mb-32 grid gap-5 items-stretch text-center py-20 lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
@@ -387,7 +387,7 @@ export default function Home() {
             Find in-depth information about Next.js features and API.
           </p>
         </a>
-        
+
         <a
           href="http://docs.artela.network/main"
           className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -404,7 +404,7 @@ export default function Home() {
             Find in-depth information about Next.js features and API.
           </p>
         </a>
-       
+
         <a
           href="http://docs.artela.network/main"
           className="group rounded-lg border border-transparent px-5 pt-8 pb-20 min-h-400 h-200 transition-colors  hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
